5c99739df2b1873439d0d7b1bab29d62c2589487
Alasdair North <alnorth@gmail.com>
2011-05-08 22:46:02 +0100

    Add simple data sending.

---
+++ extension/lrdata-interface.js
@@ -0,0 +1,21 @@
+function LRDataInterface(hostname, port) {
+	
+	function sendTrackData(track) {
+		var url = "http://"+ hostname +":"+ port +"/addtrackplay/?callback=?";
+		$.getJSON(
+			url,
+			{
+				id: track.id,
+				artist: track.artist,
+				title: track.title,
+				album: track.album,
+				filename: track.filename,
+				user: track.user,
+				userId: track.userId,
+				room: track.room,
+				timestamp: (new Date()).getTime()
+			}
+		);
+	}
+	this.sendTrackData = sendTrackData;
+}
