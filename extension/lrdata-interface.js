c92cdc9ee4bf21880da9ca7c94be5baecbcd684a
Alasdair North <alnorth@gmail.com>
2011-05-10 08:14:19 +0100

    Don't send to data server if missing vital params.

--- extension/lrdata-interface.js
+++ extension/lrdata-interface.js
@@ -1,23 +1,25 @@
 function LRDataInterface(hostname, port) {
 	
 	function sendTrackData(track) {
-		var url = "http://"+ hostname +":"+ port +"/addtrackplay/?callback=?";
-		$.getJSON(
-			url,
-			{
-				id: track.id,
-				artist: track.artist,
-				title: track.title,
-				album: track.album,
-				filename: track.filename,
-				user: track.user,
-				userId: track.userId,
-				room: track.room,
-				timestamp: track.timestamp,
-				reportedByUser: track.reportedByUser,
-				reportedByUserId: track.reportedByUserId
-			}
-		);
+		if(track.title && track.title != "" && track.artist && track.artist != "") {
+			var url = "http://"+ hostname +":"+ port +"/addtrackplay/?callback=?";
+			$.getJSON(
+				url,
+				{
+					id: track.id,
+					artist: track.artist,
+					title: track.title,
+					album: track.album,
+					filename: track.filename,
+					user: track.user,
+					userId: track.userId,
+					room: track.room,
+					timestamp: track.timestamp,
+					reportedByUser: track.reportedByUser,
+					reportedByUserId: track.reportedByUserId
+				}
+			);
+		}
 	}
 	this.sendTrackData = sendTrackData;
 }
